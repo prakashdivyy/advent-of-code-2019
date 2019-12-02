1099f9a8a231135e6a66b8c8305ba30770df8512
Prakash Divy <prakashdivyy@gmail.com>
2019-12-03 00:27:24 +0700

    Finish 01 part 2

--- 01/run.py
+++ 01/run.py
@@ -1,12 +1,22 @@
 import math
 
-total = 0
+total_part_1 = 0
+total_part_2 = 0
 
 filename = 'input.txt'
 with open(filename,) as file_object:
     for line in file_object:
-        x = int(line,10)
-        res = x / 3
-        total += math.floor(res) - 2
+        # Part 1
+        x_part_1 = int(line,10)
+        res_part_1 = x_part_1 / 3
+        total_part_1 += math.floor(res_part_1) - 2
 
-print(total)
+        # Part 2
+        x_part_2 = int(line,10)
+        while math.floor(x_part_2 / 3) > 2:
+            res_part_2 = x_part_2 / 3
+            x_part_2 = math.floor(res_part_2) - 2
+            total_part_2 += x_part_2
+
+print("Part 1: " + str(total_part_1))
+print("Part 2: " + str(total_part_2))
