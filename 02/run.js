0f8ecd2f98c0c00b2b9565f028d1515243937226
Prakash Divy <prakashdivyy@gmail.com>
2019-12-03 00:45:09 +0700

    Finish 02 part 2

--- 02/run.js
+++ 02/run.js
@@ -13,6 +13,28 @@ function setNumber(number, newNumber, array) {
   array[x][y] = newNumber;
 }
 
+function runCommand(chunkedArr) {
+  const cloneArr = JSON.parse(JSON.stringify(chunkedArr));
+  for (let ii = 0; ii < cloneArr.length; ii++) {
+    const arr = cloneArr[ii];
+    const number1 = getNumber(arr[1], cloneArr);
+    const number2 = getNumber(arr[2], cloneArr);
+
+    switch (arr[0]) {
+      case 1:
+        const newNumber1 = number1 + number2;
+        setNumber(arr[3], newNumber1, cloneArr);
+        break;
+      case 2:
+        const newNumber2 = number1 * number2;
+        setNumber(arr[3], newNumber2, cloneArr);
+        break;
+      case 99:
+        return cloneArr[0][0];
+    }
+  }
+}
+
 const readInterface = readline.createInterface({
   input: fs.createReadStream("./input.txt"),
   console: false
@@ -39,23 +61,19 @@ readInterface.on("line", function(line) {
     chunkedArr.push(modifiedArr.slice(i, i + chunk));
   }
 
-  for (let ii = 0; ii < chunkedArr.length; ii++) {
-    const arr = chunkedArr[ii];
-    const number1 = getNumber(arr[1], chunkedArr);
-    const number2 = getNumber(arr[2], chunkedArr);
+  // Part 1
+  console.log("Part 1: " + runCommand(chunkedArr));
 
-    switch (arr[0]) {
-      case 1:
-        const newNumber1 = number1 + number2;
-        setNumber(arr[3], newNumber1, chunkedArr);
+  // Part 2
+  for (let aa = 0; aa < 100; aa++) {
+    for (let bb = 0; bb < 100; bb++) {
+      const cloneArr = JSON.parse(JSON.stringify(chunkedArr));
+      cloneArr[0][1] = aa;
+      cloneArr[0][2] = bb;
+      if (runCommand(cloneArr) === 19690720) {
+        console.log("Part 2: " + (100 * aa + bb));
         break;
-      case 2:
-        const newNumber2 = number1 * number2;
-        setNumber(arr[3], newNumber2, chunkedArr);
-        break;
-      case 99:
-        console.log(chunkedArr[0][0]);
-        return;
+      }
     }
   }
 });
